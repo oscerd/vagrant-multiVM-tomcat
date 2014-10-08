bba6f091b7a77063b69fab3e8938379e2fd10011
ancosen <ancosen@gmail.com>
2014-10-08 22:02:40 +0200

    First commit

---
+++ modules/tomcat/manifests/config.pp
@@ -0,0 +1,8 @@
+# tomcat::config contains the path of all the conf/ folder file
+
+class tomcat::config{
+  $server_xml = "/conf/server.xml"
+  $context_xml = "/conf/context.xml"
+  $users_xml = "/conf/tomcat-users.xml"
+  $context_path = "/conf/Catalina/localhost/"
+}
